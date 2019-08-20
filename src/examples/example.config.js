de9c773cf32d59cb1dfef485b876add7d5a8cbe7
Damian Silbergleith Cunniff <dcunniff@gmail.com>
2019-08-20 07:29:23 -0700

    add some documentation to the config

--- src/examples/example.config.js
+++ src/examples/example.config.js
@@ -1,3 +1,8 @@
+/*
+Copy this file to "config.js" and replace your
+username/password.  Then rebuild and run
+the examples
+ */
 const config = {
   username: "you@example.com",
   password: "YOUR_NISSAN_PASS"
